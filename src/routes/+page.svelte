2c0c3c29786a8d155da19f035f601e1b5cbe8400
kaiesmahmud <kaiesmahmud@gmail.com>
2024-12-05 13:21:07 +0600

    dnd setup done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,40 @@
 <script>
-    import {Button} from "@/components/ui/button";
-    import PreviewBlogJson from "./PreviewBlogJson.svelte";
+	import { Button } from "@/components/ui/button";
+	import PreviewBlogJson from "./PreviewBlogJson.svelte";
 
+	const allButtons = ["h1", "h2", "h3", "h4", "h5", "h6", "p", "img", "list"];
 
-	const allButtons = [
-		"h1",
-		"h2",
-		"h3",
-		"h4",
-		"h5",
-		"h6",
-		"p",
-		"img",
-		"list",
-	]
+	import { writable } from "svelte/store";
+	import ParentDnd from "./ParentDND.svelte";
+
+	let nodes = [
+		{ id: 1, name: "h1" }, 
+	];
+	let dndResult = writable(nodes);
+
+	const addNewItem = (/** @type {any} */ name) => {
+		//generate id
+		// @ts-ignore
+		let items = $dndResult
+		let id = items.length + 1;
+		// check id
+		// @ts-ignore
+		if (!$dndResult[id]) {
+			//Add New Page to Main Obj
+			// @ts-ignore
+			items = [...items, { id, name }];
+			// @ts-ignore
+		} else {
+			// @ts-ignore
+			alert("Duplicate  id !");
+		}
+
+		// @ts-ignore
+		dndResult.update(() => items);
+		// @ts-ignore
+	};
+
+	dndResult.subscribe((va)=>console.log(" update ",va))
 </script>
 
 <svelte:head>
@@ -22,12 +43,26 @@
 </svelte:head>
 
 <section class="w-full min-h-screen grid grid-cols-12 border gap-5 p-5">
-	 <div class="flex flex-col gap-2 p-3">
+	<div class="flex flex-col gap-2 p-3">
 		{#each allButtons as item}
-			<Button variant="outline" class="flex gap-3 font-bold  p-5" title={item}>
+			<Button
+				on:click={()=>addNewItem(item)}
+				variant="outline"
+				class="flex gap-3 font-bold  p-5"
+				title={item}
+			>
 				<span class="text-xl text-slate-700">
-					<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...$$props}>
-						<path fill="currentColor" d="M11 13H5v-2h6V5h2v6h6v2h-6v6h-2z" />
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="1em"
+						height="1em"
+						viewBox="0 0 24 24"
+						{...$$props}
+					>
+						<path
+							fill="currentColor"
+							d="M11 13H5v-2h6V5h2v6h6v2h-6v6h-2z"
+						/>
 					</svg>
 				</span>
 				<span class=" uppercase">
@@ -35,11 +70,10 @@
 				</span>
 			</Button>
 		{/each}
-	 </div>
-	 <div class="col-span-5 border rounded p-5 bg-muted/50">
-		<h1 class="text-xl font-bold">Blog Page Create</h1>
 	</div>
-	<PreviewBlogJson/>
-	
+	<div class="col-span-5 border rounded p-5 bg-muted/50 space-y-2">
+		<h1 class="text-xl font-bold">Blog Page Create</h1>
+		<ParentDnd {dndResult} items={$dndResult} />
+	</div>
+	<PreviewBlogJson />
 </section>
- 
---
+++ src/routes/ParentDND.svelte
@@ -0,0 +1,49 @@
+<script>
+    import { flip } from "svelte/animate";
+    import { dndzone } from "svelte-dnd-action";
+    // @ts-ignore
+    export let dndResult, items;
+
+    const flipDurationMs = 300;
+    function handleDndConsider(e) {
+        items = e.detail.items;
+    }
+    function handleDndFinalize(e) {
+        items = e.detail.items;
+        dndResult.update(() => items);
+    }
+</script>
+
+<section
+    class="bg-background"
+    use:dndzone={{ items, flipDurationMs }}
+    on:consider={handleDndConsider}
+    on:finalize={handleDndFinalize}
+>
+    {#each items as item (item.id)}
+        <div animate:flip={{ duration: flipDurationMs }} class="p-5 border rounded ">
+            {#if item.name == "h1"}
+                <h1>H1</h1>
+            {:else if item.name == "h2"}
+                <h2>H2</h2> 
+            {:else if item.name == "h3"}
+                <h3>H3</h3> 
+            {:else if item.name == "h4"}
+                <h4>H4</h4> 
+            {:else if item.name == "h5"}
+                <h5>H5</h5> 
+            {:else if item.name == "h6"}
+                <h6>H6</h6> 
+            {:else if item.name == "p"}
+                <p>Paragraph</p> 
+            {:else if item.name == "img"}
+                <p>img</p> 
+            {:else if item.name == "list"}
+                <p>list</p> 
+            {:else}
+                false component
+            {/if}
+            <!-- {item.name} -->
+        </div>
+    {/each}
+</section>
